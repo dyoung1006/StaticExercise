069e95eb64038172b1aae08b1687803f19b6388a
Damien Young <dyoung1006@gmail.com>
2022-09-05 11:02:07 -0700

    final changes

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celsius {TempConverter.FahrenheitToCelsius(68)}");
+            Console.WriteLine($"fahrenheit {TempConverter.CelsiusToFahrenheit(20)}");
+        }
+    }
+}
